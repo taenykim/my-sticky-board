47699bf8527cfc7fc26248ba7c98748329b13a37
taeeun kim <kimtaeeeny@gmail.com>
2020-08-25 21:42:19 +0900

    npx create-stickyboard-app & add my card component

---
+++ src/components/sticker/CardComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { CardList } from '@stickyboard/card-list';
+
+function MyComp(props) {
+    const { colors } = props;
+
+    return <CardList />;
+}
+
+export default MyComp;
